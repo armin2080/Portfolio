e75ea868aa405b47ef70f388f19b98236556b065
armin2080 <78423228+armin2080@users.noreply.github.com>
2025-05-04 11:32:11 +0200

    Update settings.py

--- Personal_Portfolio/settings.py
+++ Personal_Portfolio/settings.py
@@ -133,8 +133,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = 'arminmaddah.a@gmail.com'
-EMAIL_HOST_PASSWORD = 'ycflmmemkxdcuawr'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'app password'
 
 
 # media files settings
