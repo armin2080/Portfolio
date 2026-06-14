77a59601f6f5299f1ebc3d3b68bbfd7902977f23
armin2080 <arminmaddah.a@gmail.com>
2026-06-14 16:57:36 +0200

    fix: use absolute path for templates DIR

--- Personal_Portfolio/settings.py
+++ Personal_Portfolio/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'Personal_Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
